e15f6867497f7b1c5c49de7e6221aa09f1cf56f0
Alexandre Gonçalves <agoncalves@ekan.com.br>
2023-01-05 14:51:11 -0300

    Adicionando alguns detalhes nas telas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Home } from "./screens/Home";
+import { Login } from "./screens/Login";
+
+import './App.css'
+import { useState } from "react";
 
 export function App() {
 
+  const [isLogged, setIsLogged] = useState(false);
+
   return (
-   <></>
+   isLogged ? <Home setIsLogged={setIsLogged}/> : <Login setIsLogged={setIsLogged}/>
   )
 }
 
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,10 +1,19 @@
 import './styles.css'
 
-export function Login() {
+type LoginProps = {
+    setIsLogged: (value: boolean) => void;
+}
+
+export function Login({setIsLogged} : LoginProps) {
+
+    function login(){
+        setIsLogged(true)
+    }
+
     return (
        <div className="containerLogin">
         <div className="card">
-        <h3 className="title">Entrar</h3>
+        <h3 className="title">Login</h3>
         <form className="form">
             <div className="inputContainer">
                 <label htmlFor="email">Email</label>
@@ -14,8 +23,16 @@ export function Login() {
                 <label htmlFor="pass">Senha</label>
                 <input type="password" id='pass' />
             </div>
-
-            <button className="button">Entrar</button>
+            <div className='options'>
+            <div className="checkboxContainer">
+                <input className='checkbox' type="checkbox" id='check' />
+                <label className='labelCheck' htmlFor="check">Lembrar senha</label>
+            </div>
+            <a className='forgotPass' href="#">Esqueceu a senha?</a>
+            </div>
+            <button onClick={login} className="button">Entrar</button>
+            <p className="register">Novo? <a href='#' className="register">Crie uma conta</a></p>
+           
         </form>
         </div>
        </div>
